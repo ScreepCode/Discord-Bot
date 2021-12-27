d75067868d6fa21dad230875d0791bc430d337d0
Niklas Buse <nicki_bu@gmx.de>
2021-12-27 20:11:01 +0100

    Remove: Token in Secrets

--- src/de/nicki/core/Secrets.java
+++ src/de/nicki/core/Secrets.java
@@ -3,8 +3,7 @@ package de.nicki.core;
 public class Secrets {
 
 	public  String getToken() {
-		//String token = "NzQxNzQ4NTYyOTUzNzY0OTM2.Xy8FLg.OmshBQQb-BizPeUkOrVo1ZzRawM";
-		String token = "NjczNjcyMjk2Mjk2MjE4NjI0.XkCsKA.bo1ckes6f4KpY5xFb9ReszZpOpE"; //ChillLouge
+		String token = "[DISCORD APPLICATION TOKEN]";
 		return token; 
 	}
 }
